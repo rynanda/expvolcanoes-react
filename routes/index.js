da76ca9959a0fce8c7ecfcc0d147114107da0179
Ryan Indrananda <ryan.indrananda@gmail.com>
2024-05-16 18:05:27 +1000

    Added countries and volcanoes route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,117 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/countries', function (req, res, next) {
+  req.db
+    .from("data")
+    .select("country")
+    .orderBy("country", "asc")
+    .distinct("country")
+    .then((rows) => {
+      const countries = rows.map((row) => row.country);
+      res.status(200).json(countries);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({
+        error: true,
+        message: "Invalid query parameters. Query parameters are not permitted."
+      })
+    });
+});
+
+router.get('/volcanoes', function (req, res, next) {
+  const country = req.query.country;
+  const popWithin = req.query.populatedWithin;
+  const queries = Object.keys(req.query);
+  const validQueries = ['country', 'populatedWithin'];
+  const validPopWithin = ['5km', '10km', '30km', '100km'];
+
+  const queryVolcanoes = req.db.from("data").select("id", "name", "country", "region", "subregion");
+
+  if (queries.some(query => !validQueries.includes(query))) {
+    res.status(400).json({
+      error: true, message: "Invalid query parameters. Only country and populatedWithin are permitted."
+    });
+  } else if (!country && !popWithin) {
+    queryVolcanoes
+      .then((rows) => {
+        res.status(200).json(rows);
+      });
+  } else if (country && !popWithin) {
+    queryVolcanoes
+      .where("country", "=", country)
+      .then((rows) => {
+        res.status(200).json(rows);
+      });
+  } else if (country && (popWithin === "5km")) {
+    queryVolcanoes
+      .where("country", "=", country)
+      .where("population_5km", ">", 0)
+      .then((rows) => {
+        res.status(200).json(rows);
+      });
+  } else if (country && (popWithin === "10km")) {
+    queryVolcanoes
+      .where("country", "=", country)
+      .where("population_10km", ">", 0)
+      .then((rows) => {
+        res.status(200).json(rows);
+      });
+  } else if (country && (popWithin === "30km")) {
+    queryVolcanoes
+      .where("country", "=", country)
+      .where("population_30km", ">", 0)
+      .then((rows) => {
+        res.status(200).json(rows);
+      });
+  } else if (country && (popWithin === "100km")) {
+    queryVolcanoes
+      .where("country", "=", country)
+      .where("population_100km", ">", 0)
+      .then((rows) => {
+        res.status(200).json(rows);
+      });
+  } else if (!country && popWithin) {
+    res.status(400).json({ error: true, message: "Country is a required query parameter." })
+  } else if (!validPopWithin.includes(popWithin)) {
+    res.status(400).json({ error: true, message: "Invalid value for populatedWithin. Only: 5km,10km,30km,100km are permitted." })
+  }
+});
+
+router.get('/volcano/:id', function (req, res, next) {
+  const queries = Object.keys(req.query);
+
+  if (queries.length === 0) {
+    req.db
+      .from("data")
+      .select("id", "name", "country", "region", "subregion", "last_eruption", "summit",
+        "elevation", "latitude", "longitude", "population_5km",
+        "population_10km", "population_30km", "population_100km")
+      .where("id", "=", req.params.id)
+      .then((rows) => {
+        if (rows.length > 0) {
+          res.status(200).json(rows);
+        } else {
+          res.status(404).json({error: true, message: `Volcano with ID: ${req.params.id} not found.`});
+        }
+      })
+  } else {
+    res.status(400).json({ error: true, message: "Invalid query parameters. Query parameters are not permitted." })
+  }
+
+})
+
+router.get('/me', function (req, res, next) {
+  res.status(200).json({
+    name: "Ryan Indrananda",
+    student_number: "n10852565"
+  });
+  return;
 });
 
 module.exports = router;
