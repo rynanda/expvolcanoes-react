3dae353a1aa58370bdce7860dde4c0e0e07462c0
Ryan Indrananda <ryan.indrananda@gmail.com>
2024-05-22 12:25:39 +1000

    Comment authorization middleware

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,16 +1,22 @@
+// Authorization middleware
+
 const jwt = require('jsonwebtoken');
 
 const authorization = (req, res, next) => {
+    // If no authorization headers sent in the request, instantnly pass control to the next middleware
     if (!req.headers.authorization) {
         next();
+
     } else {
+        // Check if JWT Bearer authorization header is correctly formatted
         if (/^Bearer /.test(req.headers.authorization)) {
-            const token = req.headers.authorization.replace(/^Bearer /, "");
+            const token = req.headers.authorization.replace(/^Bearer /, ""); // Get token
 
             try {
+                // Verify and decode JWT token to get user email, adapted from
                 // https://stackoverflow.com/questions/68024844/how-can-get-the-property-from-result-of-jwt-verify-method-that-was-already-cre
                 const decoded = jwt.verify(token, process.env.JWT_SECRET);
-                req.email = decoded.email;
+                req.email = decoded.email; // Attach decoded user email to request
                 next();
             } catch (e) {
                 if (e.name === "TokenExpiredError") {
